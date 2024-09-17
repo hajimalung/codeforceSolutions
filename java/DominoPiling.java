060a56238afe3ab7308ba7b4be5cda6f027836a0
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2024-09-17 22:39:49 +0530

    codeforces sols

--- java/DominoPiling.java
+++ java/DominoPiling.java
@@ -1,3 +1,8 @@
+/*
+ * 
+ * there is M X N board
+ * find howmany 2 X 1 dominos can be places without overlapping and not crossing the board 
+ */
 import java.util.Scanner;
 
 public class DominoPiling {
